d30d44831549607179ccd5d002fa0f62ad1808b4
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-12-19 17:50:42 +0530

    add all data1

--- services/loader.py
+++ services/loader.py
@@ -1,11 +1,16 @@
+# services/loader.py
 import json
 import os
 
 def load_data():
-    data_str = os.getenv("HUBS_JSON")
-    if data_str:
-        return json.loads(data_str)
-    # fallback to local file
-    path = os.path.join(os.path.dirname(__file__), "../data/hubs.json")
+    # 1️⃣ First try ENV (Render / Production)
+    env_json = os.getenv("HUBS_JSON")
+    if env_json:
+        return json.loads(env_json)
+
+    # 2️⃣ Fallback to local file (Local dev)
+    base_dir = os.path.dirname(__file__)
+    path = os.path.join(base_dir, "../data/hubs.json")
+
     with open(path, "r") as f:
         return json.load(f)
